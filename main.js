609522adef9c4359fef3d11683e46c9332fa2b5e
marieparker <lmparker@byui.edu>
2020-09-14 16:49:47 -0700

    update

---
+++ main.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+      label: "Week 1 notes",
+      type: 'notes',
+      url: "week1/index.html"
+    }/*,
+    {
+      label: "Week 2 Notes",
+      type: 'notes',
+      url: "week2/index.html"
+    }*/
+  ]
+
+for (let i = 0; i < links.length; i++) {
+    let item = document.createElement('li');
+    item.setAttribute('class', links[i].type);
+    
+    let newLink = document.createElement('a');
+    newLink.setAttribute('href', links[i].url);
+    newLink.textContent = links[i].label;
+    item.appendChild(newLink);
+
+    document.getElementById('list').appendChild(item);
+}
