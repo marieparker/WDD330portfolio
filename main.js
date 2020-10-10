e14b078d3e3de51f8f9d801a5a15c1cbc8ad9374
marieparker <lmparker@byui.edu>
2020-10-10 12:47:18 -0700

    update

--- main.js
+++ main.js
@@ -13,6 +13,26 @@ const links = [
       label: "Week 3 Example and Practice",
       type: 'notes',
       url: "week3/marineindex.html"
+    },
+    {
+      label: "Week 4 Examples and Practice 1",
+      type: 'notes',
+      url: "week4/search.html"
+    },
+    {
+      label: "Week 4 Examples and Practice 2",
+      type: 'notes',
+      url: "week4/hero.html"
+    },
+    {
+      label: "Week 4 Notes",
+      type: 'notes',
+      url: "week4/index.html"
+    },
+    {
+      label: "Week 4 Examples and Practice 3",
+      type: 'notes',
+      url: "week4/dice.html"
     }
   ]
 
